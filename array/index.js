d6a1c6bf84c830c1b11857dcd541a96a45b3ee7f
= <adityd2525@gmail.com>
2024-01-23 17:35:26 +0530

    "Added Arrays"

--- array/index.js
+++ array/index.js
@@ -7,7 +7,8 @@ arr[0] = 5666
 // console.log(arr[2]);
 // console.log(arr[4]);
 console.log(arr.toString()); // / this array converts the array to the string format
-console.log(arr.join('and')) // / this array joins the array with the word you want
+console.log(arr.join(' and ')) // / this array joins the array with the word you want
 console.log(arr.pop()) // / this array pop offs the last word 
 console.log(arr.push('Aditya')) // / this array push something to array it can take anything like string intergar boolean etc,
 console.log(arr.shift()) // / this array is same as the pop but it pop offs the first string or number of the array
+console.log(arr.unshift("Jack")) // / this array add the string or number you want to the beginning of the array
