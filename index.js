4ab55bb169d3d882a1a132d5080a849618003d55
= <adityd2525@gmail.com>
2024-01-23 17:28:46 +0530

    Array and Array Types

---
+++ index.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 4, 5, 7];
+//  Index  0, 1, 2, 3, 4
+arr[0] = 5666
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr[2]);
+// console.log(arr[4]);
+console.log(arr.toString()); // / this array converts the array to the string format
+console.log(arr.join('and')) // / this array joins the array with the word you want
+console.log(arr.pop()) // / this array pop offs the last word 
+console.log(arr.push('Aditya')) // / this array push something to array it can take anything like string intergar boolean etc,
